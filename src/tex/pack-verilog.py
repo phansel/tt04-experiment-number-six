309d38118debb77c4fb4628e789627d14d2b9f5e
phansel <p@kc1g.pw>
2023-09-02 16:29:01 -0700

    24->22

--- src/tex/pack-verilog.py
+++ src/tex/pack-verilog.py
@@ -3,7 +3,7 @@
 tex = open("transforms.tex", 'r')
 ver = open("../transforms.v", 'w')
 print("trying to open transforms.tex")
-linestoread=24
+linestoread=22
 line = 0
 
 startmemline = """module memory_chars(
