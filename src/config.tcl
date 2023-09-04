cfad31daf3b1180b45f802a3e412f0960f239590
phansel <p@kc1g.pw>
2023-09-04 02:01:31 -0700

    use normal density

--- src/config.tcl
+++ src/config.tcl
@@ -12,7 +12,7 @@ set script_dir [file dirname [file normalize [info script]]]
 source $::env(DESIGN_DIR)/user_config.tcl
 
 # manually force higher density
-set ::env(PL_TARGET_DENSITY) 0.62
+# set ::env(PL_TARGET_DENSITY) 0.62
 
 # save some time
 set ::env(RUN_KLAYOUT_XOR) 0
